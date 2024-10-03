75a2d69cb120b3e6f2dca9f98bdd9eb214b9f2d6
Mason Allred <mallred@csumb.edu>
2024-10-03 12:22:40 -0700

    Merged local changes into Login branch for better organization

---
+++ project_2/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './LandingPage.css'
+
+export const LandingPage = () => {
+  return (
+    <div>
+        <div className='topBar'>
+            <div className='title'>PlotPicks</div>
+            <div className='auth-container'>
+                <div className='Login'>Login</div>
+
+                <div className='SignUp'>Sign up</div>
+            </div>
+        </div>
+        <div className='imageBackground'>
+            <div className='getStartedContatiner'>
+              <div className='DisplayText'>
+              Curate Your Dream Library, One Wish at a Time.
+              </div>
+              <div className='getstartedCont'>
+              <div className='getStarted'>
+                Get started
+              </div>
+              </div>
+              
+            </div>
+        </div>
+        <div className='aboutContainer'>
+            <h1 class="about-title">Build. Add. Share.</h1>
+            <div class="about-description">
+              <p>Create as many wish lists, add all the books you want</p>
+              <p>Add books easily from the website</p>
+              <p>Share your wish list with others</p>
+            </div>
+          </div>
+    </div>
+    
+  )
+}
