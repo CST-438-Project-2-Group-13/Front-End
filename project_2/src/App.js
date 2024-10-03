9c5d13d0cc3f9130e1abc1c8045f9e094ebbb62e
Mason Allred <mallred@csumb.edu>
2024-10-03 12:14:47 -0700

    Google OAuth dependencies added

--- project_2/src/App.js
+++ project_2/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { LoginSignup } from './Components/LoginSignup/LoginSignup';
+import { Login } from './Components/LoginSignup/Login';
+import { LandingPage } from './Components/LandingPage/LandingPage';
+import { EditUser } from './Components/EditUser/EditUser';
 
 function App() {
   return (
     <div>
-      <LoginSignup/>
+      <EditUser/>
     </div>
   );
 }
