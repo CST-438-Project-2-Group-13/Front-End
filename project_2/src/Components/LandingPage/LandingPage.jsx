eeeef1e9d2daa4ba83453a514f54e4109b25b1c5
Chgunz <andyboy420019@gmail.com>
2024-10-03 01:03:50 -0700

    Add design to the landing page

--- project_2/src/Components/LandingPage/LandingPage.jsx
+++ project_2/src/Components/LandingPage/LandingPage.jsx
@@ -14,11 +14,25 @@ export const LandingPage = () => {
         </div>
         <div className='imageBackground'>
             <div className='getStartedContatiner'>
-              <div className='getStarted'>
+              <div className='DisplayText'>
+              Curate Your Dream Library, One Wish at a Time.
               </div>
+              <div className='getstartedCont'>
+              <div className='getStarted'>
+                Get started
+              </div>
+              </div>
+              
             </div>
         </div>
-        <div className='about'>This is the about</div>
+        <div className='aboutContainer'>
+            <h1 class="about-title">Build. Add. Share.</h1>
+            <div class="about-description">
+              <p>Create as many wish lists, add all the books you want</p>
+              <p>Add books easily from the website</p>
+              <p>Share your wish list with others</p>
+            </div>
+          </div>
     </div>
     
   )
