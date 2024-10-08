17fdfeb3bf1f0064f6c58d4b94266091642e8811
Mason Allred <mallred@csumb.edu>
2024-10-08 05:14:34 -0700

    Login/Signup/Landing page functionality in progress

--- project_2/src/App.js
+++ project_2/src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { LoginSignup } from './Components/LoginSignup/LoginSignup';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './Components/LoginSignup/Login';
+import { Signup } from './Components/LoginSignup/Signup';
 import { LandingPage } from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
-    <div>
-      <LandingPage/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/landing" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
